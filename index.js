b57c4fbd054c0c0c5ac6a41d8883acb1afbe9964
Roman <roman@rapidui.io>
2017-12-28 16:50:56 +0200

    asadads

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const app = express();
 const shell = require("shelljs");
-const config = require("config.json");
-
-app.get("update", (req, res) => {
+const config = require("./configs.json");
+const fs = require("fs");
+app.get("/update", (req, res) => {
     let { app, branch = "master" } = req.query;
     const configs = config.apps[app];
     if(!configs) {
         res.send("not possible to update this app. doesn't exist");
         res.end();
     } else {
+        const pm2File = JSON.parse(fs.readFileSync("./pm2.json", "utf8"));
+        const filtered = pm2File.apps.filter((cApp) => cApp.name === app);
+        if(filtered.length === 0) {
+            pm2File.apps.push({
+                name: app,
+                scripts:`${configs.path}/index.js`,
+                watch: true
+            });
+        }
+        shell.rm("./pm2.json");
+        fs.writeFileSync("./pm2.json", JSON.stringify(pm2File));
         shell.cd(configs.path);
         shell.exec("git reset --hard");
         shell.exec(`git checkout ${branch}`);
         shell.exec(`git pull ${branch}`);
-        shell.exec(`pm2 restart ${app}`);
+        shell.exec(`pm2 reload ./pm2.json`);
         res.send("your new change is done");
         res.end();
     }
